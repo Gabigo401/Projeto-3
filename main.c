ea124f214115e4155f6d63d6b418bd914ae657e1
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-07 20:30:48 -0300

    Fazendo a função de escolhas das palavras que serão adivinhadas e lendo um arquivo txt aonde estão as palavras que serão lidas pelo comando e um if para caso ocorrer algum erro ao abrir esse arquivo

--- main.c
+++ main.c
@@ -3,6 +3,15 @@
 #include <string.h>
 #include <time.h>
 
+void escolha_De_Palavra(int tentativas_maximas, int tamanho_maximo_palavra){
+    FILE *arquivo = fopen("palavras.txt", "r");  //abrindo um arquivo txt com as palavras que vão ser escolhidas aleatoriamente para serem adivinhadas
+    
+    if(arquivo == NULL){   //if se o arquivo não for aberto
+        printf("Falha ao abrir o arquivo");  //se o arquivo não for aberto aparece a mensagem falando que deu erro e voltando ao inicio do programa
+        return;
+    }
+}
+
 int main(){
     system("pause");
     return 0;
