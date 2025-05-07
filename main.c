642d50a7f3a0c884adad532079b99b556ccea9b5
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-07 20:24:57 -0300

    função para determinar o tamanho maximo da palavra e o numero maximo de tentativas

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include <string.h>
 #include <time.h>
 
+void Tentativas_E_TamanhoMaximo(int *tentativas_maximas, int *tamanho_maximo_palavra){ //função para determinar o tamanho maximo da palavras e as tentativas
+    *tentativas_maximas = 10;  //valor de 10 para as tentativas
+    *tamanho_maximo_palavra = 100;   //valor de 100 para o tamanho da palavra
+}
+
 int main(){
     system("pause");
     return 0;
