04ec392093db324a13603876d316b77511f379ce
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-07 23:04:39 -0300

    função para guardar todas as ações do jogador e recebe um char constante mensagem, o arquivo e aberto como  um txt chamado log e depois verifica se o arquivo ffoi aberto ou não e no final o arquivo e fechado

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #include <time.h>
 
+void registrar_log(const char *mensagem) {  //função para salvar todas as ações do usuario e recebe um char constante mensagem
+    FILE *log = fopen("log.txt", "a");   //abre ou cria um txt chamado log  e o ponteiro e usado para manipular o arquivo
+    if (log != NULL) {   //if para verificar se o arquivo foi aberto
+        fprintf(log, "%s\n", mensagem);  //escreve no arquivo a mensagem
+        fclose(log);  //fecha o arquivo
+    }
+}
+
 int main(){
     system("pause");
     return 0;
