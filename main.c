0a1be94afc532984de4449a29824d410de28911b
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-07 21:08:00 -0300

    char criado para armazenar as palavras e um contador usado para contar as palavras lidas, apos isso, um laço de repetição while e um fgets para ler linha por linha do arquivo e armazenar no lista[contador] e apos isso tem o tamanho maximo que limita o numero maximo que pode ser lido, o strcspn e usado para encontrar um \n na linha lida e trocar por \0 que seria o fim do arquivo e o contador no final e incrementado e o arquivo e fechado apos a leitura

--- main.c
+++ main.c
@@ -10,6 +10,15 @@ void escolha_De_Palavra(int tentativas_maximas, int tamanho_maximo_palavra){
         printf("Falha ao abrir o arquivo");  //se o arquivo não for aberto aparece a mensagem falando que deu erro e voltando ao inicio do programa
         return;
     }
+
+    char lista[100][100];  //char para armazenar ate 100 palavras
+    int contador = 0;  //contador usado  para contar quantas palavras foram lidas do arquivo
+
+    while(fgets(lista[contador], tamanho_maximo_palavra, arquivo)){    //laço de repetição usando o fgets para ler linha por linha  do arquivo e armazenar em lista[contador] e o 'tamanho_maximo_palavra e usado para colocar um limite do tamanho da linha
+        lista[contador][strcspn(lista[contador], "\n")] = '\0';   //usei para remover o \n do final da palavra que foi lida que foi gerado por fgets, o strcspn e usado para encontrar a posição de \n e o substitui por \0 que seria o fim da string
+        contador++;  //incrementa o contador apor cada palavra lida
+    }
+    fclose(arquivo);   //fecha o arquivo apos a leitura
 }
 
 int main(){
