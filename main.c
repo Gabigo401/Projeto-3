e956b170813d4b601db7390d4c8787117d48d20e
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-07 23:26:44 -0300

    cria um struct para armazenar o estado do jogo e ja declara alguns parametros dois char com tamanho maximo de 100 chamados de palavra secreta e palavra usuario e dois int para armazenar a quantidade de tentativas restantes do usuario e a quantidade de acertos e salva tudo com o nome de 'Estado_Do_Jogo'

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #include <time.h>
 
+typedef struct{   //struct criada para salvar o estado que o jogo se encontrar
+	char palavra_secreta[100];   //char para o numero maximo de letrar para a palavra secreta
+	char palavra_usuario[100];   //char para o numero max para a palavra usuario
+	int tentativas_restantes;   //int para as tentativas do usuario
+	int acertos;   //int para os acertor
+}Estado_Do_Jogo;   //salva tudo com o nome 'Estado_Do_Jogo"
+
 int main(){
     system("pause");
     return 0;
