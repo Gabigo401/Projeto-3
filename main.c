1cb0b8a0741a180e21a73e0a9643ea49bc4293e0
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-07 23:32:15 -0300

    cria um int para armazenar a escolha do jogador e depois fiz varios printf para mostrar as instruções do jogo ao jogador alem de commando e dicar para o mesmo e no final e mostrado as opções para começar o jogo entre criar um novo jogo, carregar um jogo ou sair do programa e depois o programa le a escolha e armazena ele no endereço da variavel escolha

--- main.c
+++ main.c
@@ -4,6 +4,25 @@
 #include <time.h>
 
 int main(){
+    int escolha;    //int para armazenar uma escolha do usuario
+	printf("Seja bem-vindo ao Jogo forca\n");   //mensagem de boas vindas
+	printf("\nINSTRUÇÕES DO JOGO:\n");   //mensagem das instruções de como jogar
+    printf("--------------------------------------------------\n");
+    printf("Objetivo: Descubra a palavra secreta letra por letra.\n");   //o objetivo do jogo
+    printf("Você tem um número limitado de tentativas.\n");
+    printf("\nCOMANDOS:\n");
+    printf("- Digite uma letra para tentar revelá-la na palavra.\n");   //os comando que o usuario pode usar
+    printf("- Digite a palavra inteira se quiser tentar adivinhar de uma vez.\n");
+    printf("- Digite '1' a qualquer momento para SALVAR o jogo e sair.\n");
+    printf("\nDICAS:\n");
+    printf("- Letras corretas são reveladas na posição certa.\n");   //dicas ddo jogo
+    printf("- Letras erradas reduzem o número de tentativas restantes.\n");
+    printf("- Se todas as letras forem descobertas ou você acertar a palavra, você vence!\n");
+    printf("- Se acabar as tentativas, você perde.\n");
+    printf("--------------------------------------------------\n\n");
+    printf("1 - Novo jogo\n2 - Continuar jogo salvo\n0 - Sair\n");   //mostra as opções para o jogador entre um novo jogo, carregar um jogo salvo ou sair do programa
+	scanf("%d", &escolha);   //le e salva a escolha do jogador no endereço de escolha
+
     system("pause");
     return 0;
 }
