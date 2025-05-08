d3ee76d16fe4e9ddcb4ea0751ad9ab69abeb4822
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-07 23:22:06 -0300

    ffunção que foi usada para salvar o jogo e recebe a struct estado do jogo, o arquivo e criado para escrever nele em  binario e depois verifica se ele foi aberto, se ele foi aberto escreve no arquivo usando o endereço de 'jogo' e salva so como 1 bloco depois o arquivo e fechado, se ocorrer um erro ao salvar tambem mostra uma mensagem

--- main.c
+++ main.c
@@ -3,6 +3,18 @@
 #include <string.h>
 #include <time.h>
 
+void Salvamento_Estado_Do_Jogo(Estado_Do_Jogo jogo){ //recebe a struct estado de jogo
+	FILE *f = fopen("Salvamento.bin", "wb");   //cria um arquivo em binario para escrever nele
+	if (f != NULL){   //verifica se o arquivo foi aberto ou não
+		fwrite(&jogo, sizeof(Estado_Do_Jogo), 1, f);  //escreve no arquivo e o fwrite escreve no arquivo usando o endereço de 'jogo' e o 1 mostra que so 1 bloco e salvado
+		fclose(f);  //fecha o arquivo e mostra se foi um sucesso
+		printf("O Jogo foi salvo com sucesso\n");
+	}else {
+		printf("Erro ao salva o jogo\n");   //mostra se o arquivo não abriu
+	}
+}
+
+
 int main(){
     system("pause");
     return 0;
