3b4be9a3711884192f2566f0007745fc7162b53a
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-07 23:13:21 -0300

    o int para carregar o jogo vai retornar como 1 se for um sucesso e como 0 se ocorrer algum erro ao abrir e ele recebe a struct de estado do jogo e um ponteiro, abre o  arquivo e o le como binario e apos isso ocorre a veriicação se o arquivo foi aberto se o arquivo foi aberto ele le a struct do estadodo jogo a partir do arquivo e salva ele no endereço de 'jogo' e restaura oque ja foi salvo no final o arquivo e fechado

--- main.c
+++ main.c
@@ -3,6 +3,16 @@
 #include <string.h>
 #include <time.h>
 
+int carregamento_do_jogo(Estado_Do_Jogo *jogo){  //int que ira retornar como 1 como sucesso ou 0 como erro
+	FILE *f = fopen("Salvamento.bin", "rb");   //abre o arquivo de salvamento que esta salvo em binario
+	if(f != NULL){   //verifica se o arquivo foi aberto 
+		fread(jogo, sizeof(Estado_Do_Jogo), 1, f); //le a struct Estado_Do_Jogo a partir do arquivo f e os salva no endereço jogo e restaura oque ja foi salvo
+		fclose(f); //fecha o arquivo
+		return 1;  //se for sucesso e 1
+	}
+	return 0;  //se for erro e 0
+}
+
 int main(){
     system("pause");
     return 0;
